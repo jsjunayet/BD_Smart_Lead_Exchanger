09c68a77b5c04fc680d03b1f9f94a9a8229a8a15
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2025-10-09 12:56:38 +0600

    update code

--- BDSmartLeadExchanger_Frontend/src/components/home/FeatureSection.tsx
+++ BDSmartLeadExchanger_Frontend/src/components/home/FeatureSection.tsx
@@ -105,11 +105,12 @@ const FeaturesSection = () => {
               height="315"
               src="https://www.youtube.com/embed/1yFWqYWT66E?si=NMkLROCXXV9papRE"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
             ></iframe>
+
             {/* Close Button */}
             <button
               onClick={() => setShowVideo(false)}
